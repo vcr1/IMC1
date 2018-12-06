5524d09612382dd8e1bb437db1cb82516ee874f8
vcr1 <vtr.tadeu@hotmail.com>
2018-12-06 16:21:54 -0200

    APP calcular Imc

---
+++ app/src/main/java/com/example/victor/imc/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.victor.imc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void btnCalcularOnClick (View v) {
+        TextView lblResultado = (TextView)findViewById(R.id.lblResultado);
+        EditText txtPeso = (EditText) findViewById(R.id.txtPeso);
+        EditText txtAltura = (EditText) findViewById(R.id.txtAltura);
+
+        int peso = Integer.parseInt(txtPeso.getText().toString());
+        float altura = Float.parseFloat(txtAltura.getText().toString());
+
+        float resultado = peso / (altura * altura);
+        if(resultado < 19){
+            //abaixo
+            lblResultado.setText("Abaixo do peso!");
+        }
+        else if(resultado > 32){
+            //obeso
+            lblResultado.setText("Acima do peso!");
+        }
+        else{
+            //ok
+            lblResultado.setText("Peso ideal!");
+
+    }
+}
